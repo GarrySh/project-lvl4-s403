2d4f8bb940173985f9b833ea94b5413d572c76e8
Garry Sh <sooo.lame@gmail.com>
2019-04-15 22:50:41 +1000

    change decorators

--- src/decorators.js
+++ src/decorators.js
@@ -2,11 +2,9 @@ import { reduxForm } from 'redux-form';
 import { connect as reduxConnect } from 'react-redux';
 import * as actionCreators from './actions';
 
-export const withConnect = mapStateToProps => Component =>
-  reduxConnect(mapStateToProps, actionCreators)(Component);
+export const withConnect = mapStateToProps => reduxConnect(mapStateToProps, actionCreators);
 
-export const withForm = (formName, validate) => Component =>
+export const withForm = formName =>
   reduxForm({
     form: formName,
-    validate,
-  })(Component);
+  });
