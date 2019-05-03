e27ed81620c56167254a6da6715bf844731c2a47
Garry Sh <sooo.lame@gmail.com>
2019-05-03 15:45:14 +1000

    add webpack bundle analyzer in dev mode

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,6 +1,7 @@
 import path from 'path';
 import precss from 'precss';
 import autoprefixer from 'autoprefixer';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 
 const isDevelopment = process.env.NODE_ENV === 'development';
 
@@ -43,4 +44,5 @@ export default () => ({
       },
     ],
   },
+  plugins: [isDevelopment && new BundleAnalyzerPlugin()].filter(Boolean),
 });
