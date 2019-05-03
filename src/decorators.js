e4c11365ea242d5929ff0be832fd2b354309719c
Garry Sh <sooo.lame@gmail.com>
2019-05-03 16:23:00 +1000

    delete unused decorator

--- src/decorators.js
+++ src/decorators.js
@@ -1,10 +1,6 @@
-import { reduxForm } from 'redux-form';
 import { connect as reduxConnect } from 'react-redux';
 import * as actionCreators from './actions';
 
 export const withConnect = mapStateToProps => reduxConnect(mapStateToProps, actionCreators);
 
-export const withForm = formName =>
-  reduxForm({
-    form: formName,
-  });
+export default withConnect;
